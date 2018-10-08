e8dd6867c2dc971437c3d642dacfd97b0ce008f7
flpegomes <felipegomesfurtado@gmail.com>
2018-10-08 10:15:24 -0300

    falta os 2 videos do final

---
+++ src/components/Tweet.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatTweet } from '../utils/helpers'
+
+class Tweet extends Component {
+    render() {
+        const { tweet } = this.props;
+
+        if(tweet === null ) {
+            return (
+                <div>This tweet doesn't exist</div>
+            )
+        }
+        
+        return (
+            <div class='tweet'>
+            
+            </div>
+        )
+    }
+}
+
+function mapStateToProps ({ authedUser, users, tweets }, { id }) {
+    const tweet = tweets[id]
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null
+
+    return {
+        authedUser,
+        tweet: tweet ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet) : null
+    }
+}
+
+export default connect(mapStateToProps)(Tweet)
