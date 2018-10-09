50e45f2f6b749f68f67dd8eb7715add74fe8d6d8
flpegomes <felipegomesfurtado@gmail.com>
2018-10-08 22:22:30 -0300

    Termino da aula 7.13

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti/index'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
     
@@ -19,7 +20,8 @@ class Tweet extends Component {
 
     toParent = (e, id) => {
         e.preventDefault()
-        // fazer: redirecionar para o tweet pai
+        
+        this.props.history.push(`/tweet/${id}`)
     }
 
     render() {
@@ -34,7 +36,7 @@ class Tweet extends Component {
         const { name, avatar, timestamp, text, hasLiked, likes, replies, id, parent } = tweet
 
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img
                     src={avatar}
                     alt={`Avatar of ${name}`} 
@@ -63,7 +65,7 @@ class Tweet extends Component {
                     </div>
 
                 </div>
-            </div> 
+            </Link> 
         )
     }
 }
@@ -78,4 +80,4 @@ function mapStateToProps ({ authedUser, users, tweets }, { id }) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
